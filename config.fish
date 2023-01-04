9a225d92d4a56b337d052e94e1c9a428b87df90e
Przemysław Piotrowski <przemyslaw.piotrowski@atteo.com>
2023-01-04 16:30:30 +0100

    Add Go path in config.fish Setup envsource function in fish

--- config.fish
+++ config.fish
@@ -6,4 +6,13 @@ source ~/.asdf/asdf.fish
 
 set -x PATH $PATH ~/.local/bin
 set -x PATH $PATH ~/.ebcli-virtual-env/executables
+set -x PATH $PATH ~/go/bin
 set -x EDITOR nvim
+
+function envsource
+  for line in (cat $argv | grep -v '^#')
+    set item (string split -m 1 '=' $line)
+    set -gx $item[1] $item[2]
+    echo "Exported key $item[1]"
+  end
+end
